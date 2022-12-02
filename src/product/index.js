e9777bada8e00fdeccccf7ab8075366340f13379
mymak2rs <mymak2rs@gmail.com>
2022-12-02 12:47:28 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,44 @@
+import "./index.css";
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProducts] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://feab32f5-873d-442c-9f9d-541f62e924ae.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProducts(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2022년 12월 2일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
